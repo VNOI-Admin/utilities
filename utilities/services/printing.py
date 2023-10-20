07ece1571da91e70f3197dfa1acfb8c7a07c5018
jalsol <jalsol@pm.me>
2023-10-20 09:50:50 +0700

    Add logging to services

--- utilities/services/printing.py
+++ utilities/services/printing.py
@@ -13,4 +13,5 @@ class PrintingService(Service):
 
     @rpc_method
     def print(self, source):
+        print(f'[PRINT] Printing {source}')
         subprocess.Popen(f'lpr -o media=A4 -o prettyprint -o page-border=single -o fit-to-page {source}')
--- utilities/services/user.py
+++ utilities/services/user.py
@@ -64,14 +64,16 @@ class UserRPCServiceServer(RPCServiceServer):
     @db_session
     def ping(self):
         while True:
-            print("ping")
+            print(f'[PING] Pinging {self.remote_address.host}')
             ping_ = round(ping(self.remote_address.host) * 1000, 3)  # Ping in milliseconds
             if not ping_:
                 self.user.is_online = False
                 self.user.ping = -1.0
+                print(f'[PING] {self.remote_address.host} is offline')
 
             if not self.user.is_online:
                 self.user.is_online = True
+                print(f'[PING] {self.remote_address.host} is online')
             self.user.ping = ping_
 
             gevent.sleep(config['ping_interval'])
@@ -86,6 +88,7 @@ class UserRPCServiceServer(RPCServiceServer):
         self._ping.kill()
         self._ping = None
         self.user.set_offline()
+        print(f'[DISCONNECT] Disconnecting from {self.remote_address.host}')
         return super().disconnect()
 
     def process_data(self, data):
@@ -94,6 +97,7 @@ class UserRPCServiceServer(RPCServiceServer):
         return data
 
     def process_incoming_request(self, request):
+        print(f'[REQUEST] Processing request from {self.remote_address.host}')
         if not {'__id', '__method', '__params'}.issubset(request.keys()):
             self.disconnect()
             return
@@ -125,13 +129,17 @@ class UserRPCServiceServer(RPCServiceServer):
                     response['__error'] = "%s: %s\n%s" % (error.__class__.__name__, error, error.__traceback__)
 
         try:
+            print(f'[RESPONSE] Encoding response for {self.remote_address.host}')
             data = json.dumps(response).encode('utf-8')
         except (TypeError, ValueError):
+            print(f'[ERROR] Failed to encode response for {self.remote_address.host}')
             return
 
         try:
+            print(f'[RESPONSE] Sending response to {self.remote_address.host}')
             self._write(data)
         except OSError:
+            print(f'[ERROR] Failed to write response for {self.remote_address.host}')
             return
 
 
@@ -144,12 +152,17 @@ class UserService(Service):
 
     @db_session
     def _connection_handler(self, sock: socket.socket, addr):
+        print(f'[INFO] Handling connection from {addr[0]}:{addr[1]}')
         user = User.get(ip_address=addr[0])
         if user is None:
+            print(f'[INFO] Cannot find user with IP {addr[0]}')
+            print(f'[INFO] Closing connection from {addr[0]}:{addr[1]}')
             sock.shutdown(socket.SHUT_RDWR)
             sock.close()
             return
 
+        print(f'[INFO] Found user {user.username} with IP {addr[0]}')
+        print(f'[INFO] Creating remote service for {user.username}')
         address = Address(addr[0], addr[1])
         remote_service = UserRPCServiceServer(self, address)
         remote_service.handle(sock, user)
@@ -161,20 +174,26 @@ class UserService(Service):
         return True
 
     def run(self):
+        print('[INFO] Starting API server')
         self._api.start()
         return super().run()
 
     def exit(self):
+        print('[INFO] Stopping API server')
         self._api.stop()
         super().exit()
 
     @db_session
     @rpc_method
     def set_machine_info(self, cpu, mem, user: User):
+        print(f'[INFO] Setting machine info for {user.username}')
+        print(f'[INFO] CPU: {cpu}, RAM: {mem}')
         user.cpu = cpu
         user.ram = mem
         return True
 
     @db_session
     def register_print_job(self, caller: User, source: str):
+        print(f'[PRINT] Registering print job for {caller.username}')
+        print(f'[PRINT] Source: {source}')
         Printing(caller=caller, source=source)
